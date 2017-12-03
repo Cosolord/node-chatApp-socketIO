1918506bb01762a204bd6d8fea23577482c32d04
Cosolord <cosolord@gmail.com>
2017-12-03 20:26:59 +0100

    Greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -1,8 +1,8 @@
-const path     = require('path'),
-      http     = require('http'),
-      express  = require('express'),
-      hbs      = require('handlebars'),
-      socketIO = require('socket.io');
+const path = require('path'),
+  http = require('http'),
+  express = require('express'),
+  hbs = require('handlebars'),
+  socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
@@ -11,19 +11,36 @@ var server = http.createServer(app);
 var io = socketIO(server);
 app.use(express.static(publicPath));
 
-io.on("connection", function(socket){
+io.on("connection", function(socket) {
   console.log("New user connected");
 
-  socket.on("createMessage", function(message){
+  socket.emit("newMessage", {
+    from: "Admin",
+    text: "Welcome to the chat app",
+  });
+
+  socket.broadcast.emit("newMessage", {
+    from: "System",
+    text: "New user joined",
+    createdAt: new Date().getTime()
+  });
+
+
+  socket.on("createMessage", function(message) {
     console.log("createMessage", message);
     io.emit("newMessage", {
       from: message.from,
       text: message.text,
       createdAt: new Date().getTime()
     });
+    // socket.broadcast.emit("newMessage", {
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime()
+    // });
   });
 
-  socket.on("disconnect", function(){
+  socket.on("disconnect", function() {
     console.log("User disconnected");
   });
 });
